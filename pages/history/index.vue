6ed456cbde25111ae4d8012903a72d371fc85dc3
Kananek Thongkam <info.dvgamer@gmail.com>
2019-03-19 16:47:06 +0700

    version page created

--- pages/history/index.vue
+++ pages/history/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container pt-5 pb-3">
-    <h3>History Servey</h3>
+    <h3>History Survey</h3>
     <div class="row mt-3">
       <div class="col-36">
         <table class="table table-sm table-hover">
@@ -11,23 +11,22 @@
               <th scope="col">Name</th>
               <th scope="col" class="text-center" style="width: 80px">Fail</th>
               <th scope="col" class="text-center" style="width: 160px">Created</th>
+              <th scope="col" class="text-center" style="width: 160px">Updated</th>
               <th scope="col" class="text-center" style="width: 80px" />
             </tr>
           </thead>
           <tbody>
-            <tr v-for="e in history" :key="e.nRow" :class="e.nFail > 0 ? 'table-warning' : ''">
+            <tr v-for="e in history" :key="e.nRow" :class="e.nFail > 0 ? 'table-warning' : ''" @click.prevent="onView(e.sKey)">
               <td class="text-center"><fa :icon="e.nFail > 0 ? 'times' : 'check'" :class="e.nFail > 0 ? 'text-danger' : 'text-success'" /></td>
               <th class="text-center" scope="row" v-text="e.nRow" />
               <td v-text="e.sName" />
               <td class="text-center" v-text="e.nFail" />
-              <td class="text-center" v-text="e.dCheckIn" />
+              <td class="text-center" v-text="e.dCreated" />
+              <td class="text-center" v-text="e.dModified" />
               <td class="text-center">
-                <nuxt-link 
-                  tag="button" type="button" class="btn btn-sm btn-icon"
-                  :to="{ name: 'history-id', params: { id: e.sKey }}"
-                >
+                <button type="button" class="btn btn-sm btn-icon" @click.prevent="onEdit(e.sKey)">
                   <fa icon="edit" />
-                </nuxt-link>
+                </button>
               </td>
             </tr>
           </tbody>
@@ -39,17 +38,30 @@
 <script>
 export default {
   data: () => ({
-    history: []
+    history: [],
+    editor: false
   }),
   async asyncData ({ $axios }) {
     let { data } = await $axios('/api/history')
     return { history: data }
+  },
+  methods: {
+    onView (e) {
+      if (!this.editor) this.$router.push({ name: 'history-version-id', params: { id: e } })
+    },
+    onEdit (e) {
+      this.editor = true
+      this.$router.push({ name: 'history-edit-id', params: { id: e } })
+    }
   }
 }
 </script>
 <style>
 .btn-icon {
   padding: .09rem 0.3rem;
+}
+tbody > tr {
+  cursor: pointer;
 }
 </style>
 
--- pages/history/version/_id.vue
+++ pages/history/version/_id.vue
@@ -1,6 +1,97 @@
+<template>
+  <div class="container pt-5 pb-3">
+    <no-ssr>
+      <div class="row">
+        <div class="col-sm-36">
+          <h3>History Version</h3>
+          <small>by <b>{{ editor }}</b> at {{ getTaskDateTime }}</small>
+          <hr>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-36">
+          <div v-for="(e, i) in getLastVersion()" :key="e.nTaskId">
+            <b-form-group :label-for="'chkTaskList' + e.nTaskId">
+              <fa :class="!e.problem ? 'text-pass' : 'text-fail'" :icon="!e.problem ? 'check-square' : 'window-close'" />
+              <b class="history-text"><span v-text="(i + 1) + '. ' + e.sSubject" /></b>
+              <span class="history-text d-none d-md-inline" v-html="e.sDetail" />
+              <div v-if="e.nVersion !== 1" class="history-detail">
+                <div v-for="d in getDetailVersion(e.nTaskId)" :key="d.nVersion">
+                  <fa :class="!d.problem ? 'text-pass' : 'text-fail'" :icon="!d.problem ? 'check-square' : 'window-close'" />
+                  {{ parseDate(d.dCreated) }} updated by {{ d.sName }}
+                  <pre v-if="d.problem" v-html="d.reason" />
+                </div>
+              </div>
+            </b-form-group>
+          </div>
+        </div>
+      </div>
+    </no-ssr>
+  </div>
+</template>
+
 <script>
-import index from '../index.vue'
-export default index
+import moment from 'moment'
+export default {
+  data: () => ({
+    taskKey: null,
+    editor: 'Guest',
+    tasks: []
+  }),
+  computed: {
+    getTaskDateTime () {
+      return moment(this.taskKey, 'YYYYMMDDHHmmssSSS').format('DD MMMM YYYY HH:mm:ss')
+    }
+  },
+  async asyncData ({ redirect, params, $axios }) {
+    let sKey = parseInt(params.id)
+    if (sKey == NaN) return redirect('/history')
+
+    let { data } = await $axios('/api/version/' + sKey)
+    if (!data.records) return redirect('/history')
+    
+    return { editor: data.editor, tasks: data.records, taskKey: sKey }
+  },
+  methods: {
+    parseDate (date) {
+      return moment(date).format('DD MMM YYYY HH:mm:ss')
+    },
+    getLastVersion () {
+      let nTask = []
+      return this.tasks.filter(e => {
+        if (nTask.indexOf(e.nTaskId) === -1) {
+          nTask.push(e.nTaskId)
+          return true
+        } else {
+          return false
+        }
+      })
+    },
+    getDetailVersion (nTaskId) {
+      return this.tasks.filter(e => e.nTaskId === nTaskId)
+    }
+  }
+}
 </script>
 
-
+<style>
+.history-text {
+  font-family: "Segoe UI";
+  font-size: 14px;
+}
+.history-detail {
+  font-family: "Segoe UI";
+  font-size: 12px;
+  color: #525252;
+  padding-left: 20px;
+}
+.history-detail svg {
+  width: 14px !important;
+}
+.text-fail {
+  color: #ca3232
+}
+.text-pass {
+  color: #4caf50
+}
+</style>
