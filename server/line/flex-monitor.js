280026096d4f536a4157697486faeb0a2d7d6fda
Kananek Thongkam <info.dvgamer@gmail.com>
2019-03-22 17:59:16 +0700

    flex message line supported.

--- server/line/flex-monitor.js
+++ server/line/flex-monitor.js
@@ -1,6 +1,53 @@
-module.exports = (task) => {
+const moment = require('moment')
 
-  return {
+const statusColor = (e) => {
+  switch (e) {
+    case 'INFO': return '#17a2b8'
+    case 'WARN': return '#ffc107'
+    case 'FAIL': return '#dc3545'
+    case 'PASS': return '#1DB446'
+    default: return '#CCCCCC'
+  }
+}
+
+module.exports = body => {
+  let { name, tasks } = body
+
+  let totalFail = tasks.filter(e => e.status === 'FAIL').length
+  let totalWarn = tasks.filter(e => e.status === 'WARN').length
+  let totalInfo = tasks.filter(e => e.status === 'INFO').length
+
+  let topName = `Monitor DailyClose`
+  let topStatus = (totalFail > 0) ? 'FAIL' : totalWarn > 0 ? 'WARN' : totalInfo > 0 ? 'INFO' : 'PASS'
+  let topColor = statusColor(topStatus)
+  let topDate = moment().format('HH:mm, DD MMM YYYY')
+
+  tasks = tasks.map(e => {
+    let text = !e.problem ? 'PASS' : e.status
+    let color = statusColor(text)
+    let contents = [
+      {
+        type: 'box',
+        layout: 'baseline',
+        contents: [
+          { type: 'text', text: e.sSubject, color: '#333333', weight: 'bold', size: 'xxs', flex: 12 },
+          { type: 'text', text: text, color: color, weight: 'bold', size: 'xxs', align: 'end', flex: 2 }
+        ]
+      }
+    ]
+    if (e.problem) {
+      contents.push({
+          type: 'box',
+          layout: 'baseline',
+          contents: [
+            { type: 'text', text: ' ', flex: 1 },
+            { type: 'text', text: e.reason, wrap: true, color: '#999999', weight: 'bold', size: 'xxs', flex: 24 }
+          ]
+        })
+    }
+    return contents
+  })
+  let flexMessage = {
     type: 'bubble',
     body: {
       type: 'box',
@@ -10,136 +57,30 @@ module.exports = (task) => {
           type: 'box',
           layout: 'horizontal',
           contents: [
-            {
-              type: 'text',
-              text: '[WARNING]',
-              weight: 'bold',
-              color: '#ff9800',
-              size: 'sm',
-              flex: 0
-            },
-            {
-              type: 'text',
-              text: '-',
-              color: '#FFFFFF',
-              flex: 0
-            },
-            {
-              type: 'text',
-              text: 'Mornitor Daily Close',
-              weight: 'bold',
-              size: 'sm'
-            }
+            { type: 'text', text: `[${topStatus}]`, weight: 'bold', color: topColor, size: 'sm', flex: 0 },
+            { type: 'text', text: '-', color: '#FFFFFF', flex: 0 },
+            { type: 'text', text: topName, weight: 'bold', size: 'sm' }
           ]
         },
-        {
-          type: 'separator',
-          margin: 'sm'
-        },
-        {
-          type: 'box',
-          layout: 'vertical',
-          margin: 'sm',
-          spacing: 'sm',
-          contents: [
-            {
-              type: 'box',
-              layout: 'baseline',
-              spacing: 'sm',
-              contents: [
-                {
-                  type: 'text',
-                  text: 'IIS Services',
-                  color: '#333333',
-                  weight: 'bold',
-                  size: 'xxs',
-                  flex: 12
-                },
-                {
-                  type: 'text',
-                  text: 'PASS',
-                  wrap: true,
-                  color: '#1DB446',
-                  weight: 'bold',
-                  size: 'xxs',
-                  align: 'end',
-                  flex: 2
-                }
-              ]
-            },
-            {
-              type: 'box',
-              layout: 'baseline',
-              spacing: 'sm',
-              contents: [
-                {
-                  type: 'text',
-                  color: '#333333',
-                  text: 'FTP Connection',
-                  weight: 'bold',
-                  size: 'xxs',
-                  flex: 12
-                },
-                {
-                  type: 'text',
-                  text: 'WARN',
-                  wrap: true,
-                  color: '#FF9800',
-                  weight: 'bold',
-                  size: 'xxs',
-                  align: 'end',
-                  flex: 2
-                }
-              ]
-            },
-            {
-              type: 'box',
-              layout: 'baseline',
-              spacing: 'xs',
-              contents: [
-                {
-                  type: 'text',
-                  text: ' ',
-                  flex: 1
-                },
-                {
-                  type: 'text',
-                  text: 'รอไฟล์',
-                  wrap: true,
-                  color: '#999999',
-                  weight: 'bold',
-                  size: 'xxs',
-                  flex: 24
-                }
-              ]
-            }
-          ]
-        },
-        {
-          type: 'separator',
-          margin: 'sm'
-        },
+        { type: 'separator', margin: 'sm' },
+        { type: 'box', layout: 'vertical', margin: 'sm', spacing: 'xs', contents: ([]).concat(...tasks) },
+        { type: 'separator', margin: 'sm' },
         {
           type: 'box',
           layout: 'horizontal',
           margin: 'md',
           contents: [
-            {
-              type: 'text',
-              text: '21 Mar 2019, 09:50',
-              color: '#666666',
-              size: 'xxs'
-            },
-            {
-              type: 'text',
-              text: 'Kananek Thongkam',
-              color: '#aaaaaa',
-              size: 'xxs',
-              align: 'end'
-            }
+            { type: 'text', text: topDate, color: '#666666', size: 'xxs' },
+            { type: 'text', text: name, color: '#aaaaaa', size: 'xxs', align: 'end' }
           ]
         }
       ]
     }
+  }
+
+  return {
+    type: 'flex',
+    altText: `[${topStatus}] ${topName} at ${topDate} by ${name}`,
+    contents: flexMessage
   }
 }
