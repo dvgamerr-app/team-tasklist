4dffc02f7df1762552984282db5a79396b08aef9
Kananek T <info.dvgamer@gmail.com>
2019-05-07 19:39:40 +0700

    add auth router / and update package.

--- server/authication/index.js
+++ server/authication/index.js
@@ -5,9 +5,11 @@ const { Router } = require('express')
 const router = Router()
 
 router.use(bodyParser.json())
+router.get('/', require('./router/callback'))
+router.post('/', require('./router/callback'))
 router.get('/user', require('./router/user'))
 router.post('/login', require('./router/login'))
-router.post('/logout', (req, res) => res.json({}).end())
+router.post('/logout', (req, res) => res.json({ ok: true }).end())
 
 // Export the server middleware
 module.exports = () => {
